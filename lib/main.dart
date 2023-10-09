5ef91c2c4b0747f05ff51868478db527a5422a05
Sean Kang <skang@wish.com>
2023-10-09 11:42:24 -0700

    add bloc

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  void _decrementCounter() {
+        setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter--;
+    });
+  }
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -124,9 +134,20 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+
+             TextButton(
+              onPressed: _decrementCounter,
+              child: const Text(
+                "Decrement",
+                style: TextStyle(fontSize: 25),
+              ),
+              
+            )
+
           ],
         ),
       ),
+      
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
